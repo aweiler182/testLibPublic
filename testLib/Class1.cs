c20b7f2eef353806ef3112a4e5e1a889fb7e6d70
aweiler-beckman <aweiler@beckman.com>
2021-07-11 15:34:06 -0500

    inital push

---
+++ testLib/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace testLib
+{
+    public class Class1
+    {
+        public void TestPrint(string testStr)
+        {
+            var pi = MathNet.Numerics.Constants.Pi;
+            Console.Write(pi);
+        }
+    }
+}
